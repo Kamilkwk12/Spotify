8045c49afb526488b1fc3d4d706beb1e3c175f1b
Kamilkwk12 <115706153+Kamilkwk12@users.noreply.github.com>
2022-10-14 15:46:22 +0200

    Add files via upload

--- spotify/script.js
+++ spotify/script.js
@@ -1,20 +1,32 @@
+// kod odpowiedzialny za wyświetlanie menu ustawień po 
+// kliknięniu w przycisk w lewym górnym rogu strony
+var arrowDown = document.getElementById("arrow-down");
+var myDropmenu = document.getElementById("my-dropmenu");
+var arrowUp = document.getElementById("arrow-up")
+
 function menuClick() {
-    document.getElementById("my-dropmenu").classList.toggle("show");
+    myDropmenu.classList.toggle("show");
+    arrowDown.classList.toggle("hide-arrow");
+    arrowUp.classList.toggle("show-arrow")
 }
 
 window.onclick = function(event) {
-  if (!event.target.matches('.settings-btn, .drake, .arrow, .text')) {
+  if (!event.target.matches('.settings-btn, .drake, .arrow-down, .text')) {
     var dropdowns = document.getElementsByClassName("dropmenu");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
       if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+          openDropdown.classList.remove('show');
+          arrowUp.classList.toggle('show-arrow');
+          arrowDown.classList.toggle('hide-arrow');
       }
     }
   }
 }
 
+
+// kod odpowiedzialny za zmianę koloru headera przy przewijaniu strony
 var header = document.getElementById("header-div");
 
 function whereIsSiteOnScroll() {
@@ -31,4 +43,13 @@ window.onscroll = function() {
   else {
     header.classList.remove("header-background")
   }
+}
+
+//kod odpowiedzialny za timer przy player-sliderze
+var timer = document.getElementById("timer");
+var slider = document.getElementById("play-slider");
+timer.innerHTML = slider.value;
+
+slider.oninput = function() {
+  timer.innerHTML = slider.value;
 }
